fe3f352c96d0667c4727fa4be67a998657396161
Compilenix <Compilenix@compilenix.org>
2017-04-19 20:25:30 +0200

    cleanup ForeignKeyNamingConvention

--- Source/Compilenix.HttpHeaderSurvey.Implementation.DataAccess/ForeignKeyNamingConvention.cs
+++ Source/Compilenix.HttpHeaderSurvey.Implementation.DataAccess/ForeignKeyNamingConvention.cs
@@ -1,48 +1,26 @@
-﻿using System.Data.Entity.Core.Metadata.Edm;
+﻿using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
+using JetBrains.Annotations;
+// ReSharper disable All
 
 namespace Compilenix.HttpHeaderSurvey.Implementation.DataAccess
 {
     public class ForeignKeyNamingConvention : IStoreModelConvention<AssociationType>
     {
-        public void Apply(AssociationType association, DbModel model)
+        private static bool DoPropertiesHaveDefaultNames([NotNull] IReadOnlyCollection<EdmProperty> properties, [ItemNotNull] [NotNull] IReadOnlyList<EdmProperty> otherEndProperties)
         {
-            if (association == null)
-            {
-                return;
-            }
-
-            if (!association.IsForeignKey)
-            {
-                return;
-            }
-
-            var constraint = association.Constraint;
-            if (DoPropertiesHaveDefaultNames(constraint?.FromProperties, constraint?.ToRole?.Name, constraint?.ToProperties))
-            {
-                NormalizeForeignKeyProperties(constraint?.FromProperties);
-            }
-            if (DoPropertiesHaveDefaultNames(constraint?.ToProperties, constraint?.FromRole?.Name, constraint?.FromProperties))
-            {
-                NormalizeForeignKeyProperties(constraint?.ToProperties);
-            }
-        }
-
-        private bool DoPropertiesHaveDefaultNames(ReadOnlyMetadataCollection<EdmProperty> properties,
-                                                  string roleName,
-                                                  ReadOnlyMetadataCollection<EdmProperty> otherEndProperties)
-        {
-            if (properties?.Count != otherEndProperties?.Count)
+            if (properties.Count != otherEndProperties.Count)
             {
                 return false;
             }
 
-            return !properties.Where((property, i) => !property.Name.EndsWith("_" + otherEndProperties[i].Name)).Any();
+            return !properties.Where((property, i) => !property?.Name?.EndsWith("_" + otherEndProperties[i]?.Name) ?? false).Any();
         }
 
-        private void NormalizeForeignKeyProperties(ReadOnlyMetadataCollection<EdmProperty> properties)
+        private static void NormalizeForeignKeyProperties(IReadOnlyCollection<EdmProperty> properties)
         {
             if (properties == null)
             {
@@ -77,6 +55,24 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.DataAccess
                     newPropertyName = navigationPropertyName + targetKey;
                 }
                 t.Name = newPropertyName;
+            }
+        }
+
+        public void Apply([NotNull] AssociationType association, [NotNull] DbModel model)
+        {
+            if (!association.IsForeignKey)
+            {
+                return;
+            }
+
+            var constraint = association.Constraint;
+            if (DoPropertiesHaveDefaultNames(constraint?.FromProperties, constraint?.ToProperties))
+            {
+                NormalizeForeignKeyProperties(constraint?.FromProperties);
+            }
+            if (DoPropertiesHaveDefaultNames(constraint?.ToProperties, constraint?.FromProperties))
+            {
+                NormalizeForeignKeyProperties(constraint?.ToProperties);
             }
         }
     }
