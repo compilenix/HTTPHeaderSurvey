3c01e82b71ff47d3cd8f338014708f18c1d6ab8d
Compilenix <Compilenix@compilenix.org>
2017-04-19 20:33:52 +0200

    update RequestJobWorker

--- Source/Compilenix.HttpHeaderSurvey.Implementation.Domain/RequestJobWorker.cs
+++ Source/Compilenix.HttpHeaderSurvey.Implementation.Domain/RequestJobWorker.cs
@@ -12,39 +12,43 @@ using Compilenix.HttpHeaderSurvey.Integration.DataAccess.Entitys;
 using Compilenix.HttpHeaderSurvey.Integration.DataAccess.Repositories;
 using Compilenix.HttpHeaderSurvey.Integration.Domain;
 using Compilenix.HttpHeaderSurvey.Integration.Domain.Modules;
+using JetBrains.Annotations;
 
 namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
 {
+    [UsedImplicitly]
     public class RequestJobWorker : IRequestJobWorker
     {
         private readonly TimeSpan _httpClientTimeout;
         private CancellationTokenSource _cancellationTokenSource;
         private BufferBlock<RequestJob> _inputBufferBlock;
-        private ActionBlock<RequestJob> _jobCompletedBlock;
-        private List<TransformBlock<RequestJob, RequestJob>> _processJobBlocks;
+        private ActionBlock<Tuple<RequestJob, bool>> _jobCompletedBlock;
+        private List<TransformBlock<RequestJob, Tuple<RequestJob, bool>>> _processJobBlocks;
 
+        // ReSharper disable once AssignNullToNotNullAttribute
         public Task Completion => _jobCompletedBlock?.Completion;
 
+        // TODO add IUnitOfWork
         public RequestJobWorker()
         {
-            _httpClientTimeout = TimeSpan.FromSeconds(int.Parse(IoC.Resolve<IApplicationConfigurationCollection>().Get("HttpClientTimeoutSeconds")));
+            _httpClientTimeout = TimeSpan.FromSeconds(int.Parse(IoC.Resolve<IApplicationConfigurationCollection>().Get("HttpClientTimeoutSeconds") ?? "60"));
             HttpClientUtils.DefaultTimeout = _httpClientTimeout;
         }
 
-        private static async Task FillConsumer(int countOfJobsToProcess, ITargetBlock<RequestJob> targetBlock, CancellationToken token)
+        private static async Task FillConsumerAsync(int countOfJobsToProcess, [NotNull] ITargetBlock<RequestJob> targetBlock, CancellationToken token)
         {
             int itemsGot;
             do
             {
                 using (var unit = IoC.Resolve<IUnitOfWork>())
                 {
-                    var repo = unit.Repository<IRequestJobRepository>();
+                    var repo = unit.Resolve<IRequestJobRepository>();
 
                     itemsGot = 0;
                     var batchSize = countOfJobsToProcess < 1000 ? countOfJobsToProcess : 1000;
-                    var _jobs = repo.GetRequestJobsTodoAndNotScheduled(withRequestHeaders: true, count: batchSize, checkout: true);
+                    var jobs = repo.GetRequestJobsTodoAndNotScheduled(withRequestHeaders: true, count: batchSize, checkout: true);
 
-                    foreach (var job in _jobs)
+                    foreach (var job in jobs)
                     {
                         if ((targetBlock.Completion?.IsCanceled ?? true) || targetBlock.Completion.IsFaulted || token.IsCancellationRequested)
                         {
@@ -52,7 +56,13 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
                         }
 
                         itemsGot++;
-                        await targetBlock.SendAsync(job);
+
+                        // ReSharper disable once MethodSupportsCancellation
+                        var sendAsync = targetBlock.SendAsync(job);
+                        if (sendAsync != null)
+                        {
+                            await sendAsync;
+                        }
                     }
 
                     countOfJobsToProcess -= itemsGot;
@@ -66,7 +76,10 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
         public async Task StopAsync()
         {
             _cancellationTokenSource?.Cancel();
-            await _jobCompletedBlock?.Completion;
+            if (_jobCompletedBlock?.Completion != null)
+            {
+                await _jobCompletedBlock?.Completion;
+            }
         }
 
         public async Task StartAsync(int countOfJobsToProcess)
@@ -75,6 +88,8 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
             {
                 _cancellationTokenSource = new CancellationTokenSource();
                 InitDataflow();
+
+                // ReSharper disable once PossibleNullReferenceException
                 await ProcessPendingJobs(countOfJobsToProcess, _cancellationTokenSource.Token);
             }
         }
@@ -86,8 +101,16 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
             _cancellationTokenSource?.Dispose();
         }
 
-        private async Task CompleteRequestJob(RequestJob job)
+        private async Task CompleteRequestJob(Tuple<RequestJob, bool> data)
         {
+            if (data == null)
+            {
+                return;
+            }
+
+            var job = data.Item1;
+            var saveCompleted = data.Item2;
+
             if (job == null)
             {
                 return;
@@ -97,7 +120,7 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
             {
                 using (var unit = IoC.Resolve<IUnitOfWork>())
                 {
-                    job = await unit.Repository<IRequestJobRepository>().GetAsync(job.Id);
+                    job = await unit.Resolve<IRequestJobRepository>().GetAsync(job.Id);
 
                     if (job == null)
                     {
@@ -105,32 +128,41 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
                     }
 
                     job.IsCurrentlyScheduled = false;
-                    job.LastTimeProcessed = DateTime.Now;
+
+                    if (saveCompleted)
+                    {
+                        job.LastTimeProcessed = DateTime.Now;
+                    }
 
                     await unit.CompleteAsync();
 
                     // TODO remove
+                    // ReSharper disable once LocalizableElement
                     Console.WriteLine($"completed job -> {job.Uri}");
                 }
             }
             catch (Exception exception)
             {
-                this.Log()?.Error(string.Empty, exception);
+                this.Log().Error(string.Empty, exception);
             }
         }
 
         private void InitDataflow()
         {
-            var parallelism = Environment.ProcessorCount;
-            var blockOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = parallelism, BoundedCapacity = parallelism };
+            var blockOptions = new ExecutionDataflowBlockOptions
+                {
+                    MaxDegreeOfParallelism = DataflowBlockOptions.Unbounded,
+                    BoundedCapacity = 3
+                };
 
-            _inputBufferBlock = new BufferBlock<RequestJob>(new DataflowBlockOptions { BoundedCapacity = (parallelism * parallelism) << 3 });
-            _processJobBlocks = new List<TransformBlock<RequestJob, RequestJob>>();
+            _inputBufferBlock = new BufferBlock<RequestJob>(new DataflowBlockOptions { BoundedCapacity = (Environment.ProcessorCount * Environment.ProcessorCount) << 3 });
+            _processJobBlocks = new List<TransformBlock<RequestJob, Tuple<RequestJob, bool>>>();
             for (var i = 0; i < Environment.ProcessorCount << 3; i++)
             {
-                _processJobBlocks.Add(new TransformBlock<RequestJob, RequestJob>(job => ProcessRequestJob(job), blockOptions));
+                // ReSharper disable once AssignNullToNotNullAttribute
+                _processJobBlocks.Add(new TransformBlock<RequestJob, Tuple<RequestJob, bool>>(job => ProcessRequestJob(job), blockOptions));
             }
-            _jobCompletedBlock = new ActionBlock<RequestJob>(CompleteRequestJob, blockOptions);
+            _jobCompletedBlock = new ActionBlock<Tuple<RequestJob, bool>>(CompleteRequestJob, blockOptions);
 
             foreach (var jobBlock in _processJobBlocks)
             {
@@ -138,29 +170,49 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
                 jobBlock.LinkTo(_jobCompletedBlock);
 
                 // ReSharper disable once MethodSupportsCancellation
-                _inputBufferBlock.Completion.ContinueWith(t => { jobBlock.Complete(); });
+                _inputBufferBlock.Completion?.ContinueWith(t => { jobBlock.Complete(); });
             }
         }
 
         private async Task ProcessPendingJobs(int countOfJobsToProcess, CancellationToken token)
         {
-            await FillConsumer(countOfJobsToProcess, _inputBufferBlock, token);
+            if (_inputBufferBlock != null)
+            {
+                await FillConsumerAsync(countOfJobsToProcess, _inputBufferBlock, token);
 
-            await _inputBufferBlock.Completion;
-            var processJobBlockTasks = _processJobBlocks.Select(x => x.Completion);
-            Task.WaitAll(processJobBlockTasks.ToArray());
-            _jobCompletedBlock.Complete();
-            await _jobCompletedBlock.Completion;
+                if (_inputBufferBlock?.Completion != null)
+                {
+                    await _inputBufferBlock?.Completion;
+                }
+            }
+
+            var processJobBlockTasks = _processJobBlocks?.Select(x => x?.Completion);
+            if (processJobBlockTasks != null)
+            {
+                Task.WaitAll(processJobBlockTasks.ToArray());
+            }
+
+            _jobCompletedBlock?.Complete();
+            if (_jobCompletedBlock?.Completion != null)
+            {
+                await _jobCompletedBlock?.Completion;
+            }
         }
 
-        private async Task<RequestJob> ProcessRequestJob(RequestJob requestJob)
+        private async Task<Tuple<RequestJob, bool>> ProcessRequestJob([NotNull] RequestJob requestJob)
         {
             HttpResponseMessage jobResult = null;
+            var saveCompleted = true;
             try
             {
                 using (var unit = IoC.Resolve<IUnitOfWork>())
                 {
-                    requestJob = await unit.Repository<IRequestJobRepository>().GetWithRequestHeadersAsync(requestJob.Id);
+                    requestJob = await unit.Resolve<IRequestJobRepository>().GetWithRequestHeadersAsync(requestJob.Id);
+
+                    if (requestJob?.Uri == null)
+                    {
+                        return new Tuple<RequestJob, bool>(null, false);
+                    }
 
                     var httpClientOptions = new HttpClientRequestOptions
                         {
@@ -171,51 +223,45 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
                             HeadersOnly = true,
                             CancellationToken = new CancellationTokenSource(_httpClientTimeout).Token
                         };
+                    var responseMessage = new ResponseMessage { RequestJob = requestJob };
 
                     try
                     {
-                        // TODO Read header as stream
                         jobResult = await HttpClientUtils.InvokeWebRequestAsync(httpClientOptions);
                     }
                     catch (Exception exception)
                     {
-                        // TODO extend response message to include reference of exception
-                        //if (exception.GetAllMessages().Any(m => m.Contains("The remote name could not be resolved"))
-                        //    || exception.GetAllMessages()
-                        //        .Any(
-                        //            m =>
-                        //                m.Contains(
-                        //                    "A connection attempt failed because the connected party did not properly respond after a period of time"))
-                        //    || exception.GetAllMessages()
-                        //        .Any(m => m.Contains("No connection could be made because the target machine actively refused"))
-                        //    || exception.GetAllMessages().Any(m => m.Contains("was forcibly closed by the remote host"))
-                        //    || exception.GetAllMessages().Any(m => m.Contains("The connection was closed unexpectedly"))
-                        //    || exception.GetAllMessages().Any(m => m.Contains("Unable to read data from the transport connection")))
-                        //{
-                        //    this.Log()?.Error($"Error on: {requestJob.Uri}", exception);
-                        //    return requestJob;
-                        //}
+                        if (await unit.Resolve<IResponseErrorModule>().AddAsync(responseMessage, exception, unit))
+                        {
+                            unit.Resolve<IResponseMessageRepository>().Add(responseMessage);
+                            this.Log().Debug($"Known Error on: {requestJob.Uri}");
+                            return new Tuple<RequestJob, bool>(requestJob, true);
+                        }
 
-                        this.Log()?.Error($"Error on: {requestJob.Uri}", exception);
-                        return requestJob;
+                        // TODO give admin possibillity to add error to list of known errors
+                        this.Log().Error($"Error on: {requestJob.Uri}", exception);
+                        return new Tuple<RequestJob, bool>(requestJob, true);
                     }
 
-                    List<ResponseHeader> headers;
-                    using (var module = IoC.Resolve<IResponseHeaderModule>())
+                    if (jobResult.Headers == null)
                     {
-                        headers = await module.GetResponseHeadersFromListAsync(jobResult.Headers, unit);
-                        headers.AddRange(await module.GetResponseHeadersFromListAsync(jobResult.Content.Headers, unit));
+                        throw new AggregateException("http result has no response headers");
                     }
 
-                    unit.Repository<IResponseMessageRepository>()
-                        .Add(
-                            new ResponseMessage
-                                {
-                                    RequestJob = requestJob,
-                                    ResponseHeaders = headers,
-                                    ProtocolVersion = jobResult.Version.ToString(),
-                                    StatusCode = (int)jobResult.StatusCode
-                                });
+                    if (jobResult.Content?.Headers == null)
+                    {
+                        throw new AggregateException("http result has no response content headers");
+                    }
+
+                    var responseHeaderModule = unit.Resolve<IResponseHeaderModule>();
+                    var headers = await responseHeaderModule.GetResponseHeadersFromListAsync(jobResult.Headers, unit);
+                    headers.AddRange(await responseHeaderModule.GetResponseHeadersFromListAsync(jobResult.Content.Headers, unit));
+
+                    responseMessage.ResponseHeaders = headers;
+                    responseMessage.ProtocolVersion = jobResult.Version?.ToString();
+                    responseMessage.StatusCode = (int)jobResult.StatusCode;
+
+                    unit.Resolve<IResponseMessageRepository>().Add(responseMessage);
 
                     try
                     {
@@ -223,9 +269,8 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
                     }
                     catch (Exception exception)
                     {
-                        if (
-                            !exception.GetAllMessages()?
-                                .Any(e => e?.Contains($"Cannot insert duplicate key row in object 'dbo.{nameof(ResponseHeader)}s'") ?? true) ?? true)
+                        saveCompleted = false;
+                        if (!exception.GetAllMessages().Any(e => e?.Contains($"Cannot insert duplicate key row in object 'dbo.{nameof(ResponseHeader)}s'") ?? false))
                         {
                             throw;
                         }
@@ -234,14 +279,15 @@ namespace Compilenix.HttpHeaderSurvey.Implementation.Domain
             }
             catch (Exception exception)
             {
-                this.Log()?.Fatal(string.Empty, exception);
+                saveCompleted = false;
+                this.Log().Fatal(string.Empty, exception);
             }
             finally
             {
                 jobResult?.Dispose();
             }
 
-            return requestJob;
+            return new Tuple<RequestJob, bool>(requestJob, saveCompleted);
         }
     }
 }
--- Source/Compilenix.HttpHeaderSurvey.Integration.Domain/IRequestJobWorker.cs
+++ Source/Compilenix.HttpHeaderSurvey.Integration.Domain/IRequestJobWorker.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 
 namespace Compilenix.HttpHeaderSurvey.Integration.Domain
 {
+    [SuppressMessage("ReSharper", "UnusedMemberInSuper.Global")]
     public interface IRequestJobWorker : IDisposable
     {
+        [NotNull]
         Task Completion { get; }
+
+        [NotNull]
         Task StartAsync(int countOfJobsToProcess);
+
+        [NotNull]
         Task StopAsync();
     }
 }
