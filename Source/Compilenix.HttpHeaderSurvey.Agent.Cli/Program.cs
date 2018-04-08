45abc5a40e99e29a30930b8930a232a7d3d64f8d
Compilenix <compilenix@compilenix.org>
2018-04-08 21:00:42 +0200

    Update default User-Agent

--- Source/Compilenix.HttpHeaderSurvey.Agent.Cli/Program.cs
+++ Source/Compilenix.HttpHeaderSurvey.Agent.Cli/Program.cs
@@ -47,7 +47,7 @@ namespace Compilenix.HttpHeaderSurvey.Agent.Cli
             {
                 var requestHeaderModule = unit.Resolve<IRequestHeaderModule>();
 
-                var list = new List<RequestHeader> { new RequestHeader { Key = "user-agent", Value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36" }, new RequestHeader { Key = "accept-encoding", Value = "gzip, deflate, br" }, new RequestHeader { Key = "accept-language", Value = "en-US,en;q=0.8,de;q=0.6" }, new RequestHeader { Key = "upgrade-insecure-requests", Value = "1" } };
+                var list = new List<RequestHeader> { new RequestHeader { Key = "user-agent", Value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36" }, new RequestHeader { Key = "accept-encoding", Value = "gzip, deflate" }, new RequestHeader { Key = "accept-language", Value = "en-US,en;q=0.8,de;q=0.6" }, new RequestHeader { Key = "upgrade-insecure-requests", Value = "1" } };
 
                 foreach (var header in list)
                 {
